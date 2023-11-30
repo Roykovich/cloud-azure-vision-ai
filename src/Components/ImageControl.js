e8dcfd4f3bc29171766dd15b888fda992ea33588
Rubén Ortiz <runerd.menethil@gmail.com>
2023-11-30 01:44:25 -0300

    Create Image control component

---
+++ src/Components/ImageControl.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useState } from 'react';
+import AnalyzeImage from './AnalyzeImage'; 
+import GenerateImage from './GenerateImage';
+import LoadingAnimation from './LoadingAnimation';
+
+import {analyzeImage} from '../azure-image-analysis';
+import {generateImage} from '../azure-image-generation';
+
+function ImageControl() {
+  const [inputValue, setInputValue] = useState("");
+  const [imageAnalysis, setImageAnalysis] = useState(null);
+  const [imageGeneration, setImageGeneration] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleAnalysis = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    await analyzeImage(inputValue)
+    .then(response => {
+      setLoading(false)
+      setImageAnalysis(response);
+    });
+  }
+
+  const handleGeneration = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    await generateImage(inputValue)
+    .then(response => {
+      setLoading(false)
+      setImageGeneration(response);
+    });
+  }
+
+  return (    
+    <div>
+      <h1>Computer Vision</h1>
+      <form>
+        <label>Insert URL or type prompt:</label>
+        <input type="text" value={inputValue} onChange={handleChange}/>
+        <button onClick={handleAnalysis}>Analyze</button>
+        <button onClick={handleGeneration}>Generate</button>
+      </form>
+      <hr />
+      {loading && <LoadingAnimation />}
+      {imageAnalysis && <AnalyzeImage json={imageAnalysis} url={inputValue} description={imageAnalysis.description.captions[0].text}/>}
+      {imageGeneration && <GenerateImage json={imageGeneration} prompt={inputValue} url={imageGeneration[0].url} description={imageGeneration[0].revised_prompt}/>}
+    </div>
+  );
+}
+
+export default ImageControl;
